75ee9ce11d1570319df6e5f006b2ec0ae06ebebe
Preet Rana <preetrana1263@gmail.com>
2026-04-26 13:33:34 +0530

    added some api for fronted of ai and transaction

--- Frontend/src/api/aiInsightApi.js
+++ Frontend/src/api/aiInsightApi.js
@@ -4,3 +4,8 @@ export const getAiInsightApi = async () => {
     const res = await api.get('/ai/');
     return res;
 }
+
+export const getAiTip = async () => {
+    const res = await api.get('/ai/tip');
+    return res;
+}
--- Frontend/src/api/transactionApi.js
+++ Frontend/src/api/transactionApi.js
@@ -3,14 +3,24 @@ import api from "./api";
 export const getTransactionApi = () => {
     const res = api.get("/transaction/");
     return res;
-}
+};
 
 export const postTransactionApi = (formData) => {
-    const res = api.post('/transaction/', formData);
+    const res = api.post("/transaction/", formData);
     return res;
-}
+};
 
 export const updateTransactionApi = (formData, id) => {
     const res = api.put(`/transaction/${id}`, formData);
     return res;
-}
+};
+
+export const getCardInfo = async () => {
+    const res = api.get("/transaction/get-dashboard-card-info");
+    return res;
+};
+
+export const getIncomeExpense = async () => {
+    const res = api.get("/transaction/get-income-expense");
+    return res;
+};
