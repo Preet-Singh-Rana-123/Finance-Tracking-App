deeffc391d9ce6ed69d0919ca781cf30c8c30f88
Preet Rana <preetrana1263@gmail.com>
2026-06-01 16:00:09 +0530

    updated api according to minikube service

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+  baseURL: "http://127.0.0.1:53347",
 });
 
 api.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
+  const token = localStorage.getItem("token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
 });
 
 export default api;
