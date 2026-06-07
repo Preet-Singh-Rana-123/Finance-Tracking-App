e5837088134d96eef3336cb9bcf0af9463a34663
Preet Rana <preetrana1263@gmail.com>
2026-06-07 12:02:05 +0530

    updated links on navbar and configure public folder as per render deployment

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,40 +1,44 @@
+import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export const Navbar = () => {
     const user = useSelector((state) => state.auth.user);
+
     return (
-        <>
-            <div className="flex justify-between p-[1rem] shadow-md bg-gray-50 rounded-xl">
-                <h3 className="text-2xl font-bold text-blue-500">
-                    <a href="/">FinTrack</a>
-                </h3>
-                <div className="flex gap-4 mx-4 font-semibold">
-                    <a
-                        className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
-                        href="transaction"
-                    >
-                        Transactions
-                    </a>
-                    <a
-                        className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
-                        href="/budget"
-                    >
-                        Budget
-                    </a>
-                    <a
-                        className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
-                        href="/aiInsight"
-                    >
-                        AI Insightst
-                    </a>
-                    <a
-                        className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
-                        href="/login"
-                    >
-                        {user ? "Logout" : "Login"}
-                    </a>
-                </div>
+        <div className="flex justify-between p-[1rem] shadow-md bg-gray-50 rounded-xl">
+            <h3 className="text-2xl font-bold text-blue-500">
+                <Link to="/">FinTrack</Link>
+            </h3>
+
+            <div className="flex gap-4 mx-4 font-semibold">
+                <Link
+                    className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
+                    to="/transaction"
+                >
+                    Transactions
+                </Link>
+
+                <Link
+                    className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
+                    to="/budget"
+                >
+                    Budget
+                </Link>
+
+                <Link
+                    className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
+                    to="/aiInsight"
+                >
+                    AI Insights
+                </Link>
+
+                <Link
+                    className="hover:bg-gray-300 p-2 px-4 rounded-xl duration-300"
+                    to="/login"
+                >
+                    {user ? "Logout" : "Login"}
+                </Link>
             </div>
-        </>
+        </div>
     );
 };
